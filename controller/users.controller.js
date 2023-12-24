db96e41555128ccfe3e7c87c5c250aba119e847a
ibrahimk4111 <ibrahimk4111@gmail.com>
2023-12-24 11:07:04 +0600

    bootstrap added with table

--- controller/users.controller.js
+++ controller/users.controller.js
@@ -9,7 +9,7 @@ exports.getUsersForm = (req, res) => {
 
 // get users
 exports.getUsers = (req, res) => {
-  res.status(200).json({ users });
+  res.status(200).json(users);
 };
 
 // post users or create new user
@@ -27,7 +27,9 @@ exports.createUsers = (req, res) => {
     age,
   };
   users.push(user);
-  res.status(201).json({ users });
+  // res.status(201).json(users);
+  res.sendFile(path.join(__dirname + "/../views/index.html"));
+  res.status(301).redirect("/")
 };
 
 // update user's data
@@ -42,7 +44,7 @@ exports.updateUser = (req, res) => {
       filteredUser.email = email;
       filteredUser.age = age;
     });
-  res.status(200).json({ users });
+  res.status(200).json(users);
 };
 
 // delete user
@@ -50,7 +52,7 @@ exports.deleteUser = (req, res) => {
   try {
     const userId = req.params.id;
     afterDeleteUser = users.filter((user) => user.id != userId);
-    res.status(200).json({ afterDeleteUser }); 
+    res.status(200).json(afterDeleteUser); 
   } catch (error) {
     res.status(500).json("server error");  
   }
